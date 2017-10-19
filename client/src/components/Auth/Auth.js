29e8122f0ada7a96e5fdfa1d6a6c40647aba62c0
Michael Dinh <mkdinh94@gmail.com>
2017-10-19 02:25:11 -0400

    fix process.env variable for auth0

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -3,13 +3,13 @@ import auth0 from "auth0-js";
 import history from "./history";
 
 
-
+// create Authentication component
 export default class Auth {
     
     auth0 = new auth0.WebAuth({
-      domain: "app78488740.auth0.com",
-      clientID:  "doh0u_68zAATE2KyI1th-O1h8YnndJEO",
-      redirectUri: "http:localhost:3000/build",
+      domain: process.env.AUTH0_DOMAIN || "app78488740.auth0.com",
+      clientID:  process.env.AUTH0_CLIENT_ID || "doh0u_68zAATE2KyI1th-O1h8YnndJEO",
+      redirectUri: process.env.AUTH0_CALLBACK_URL || "http:localhost:3000/build",
       audience: 'https://app78488740.auth0.com/userinfo',
       responseType: 'token id_token',
       scope: 'openid'
